d10e60293235fad12f83105802dea233bb3f75d6
Kevin Benevides <39235256801@info.local>
2019-02-15 11:34:29 -0200

    att

---
+++ VisualStudio/Senai.SviGufo.WebApi/Senai.SviGufo.WebApi/Controllers/InstituicaoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Senai.SviGufo.WebApi.Domains;
+using Senai.SviGufo.WebApi.Interfaces;
+using Senai.SviGufo.WebApi.Repositorios;
+using System.Collections.Generic;
+
+namespace Senai.SviGufo.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class InstituicaoController : ControllerBase
+    {
+
+        private IInstituicaoRepository InstituicaoRepository { get; set; }
+
+        public InstituicaoController()
+        {
+            InstituicaoRepository = new InstituicaoRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<InstituicaoDomain> Get()
+        {
+            return InstituicaoRepository.Listar();
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            InstituicaoDomain instituicao = InstituicaoRepository.BuscarId(id);
+
+            return Ok(instituicao);
+
+        }
+
+        [HttpPost]
+        public IActionResult Post(InstituicaoDomain InstituicaoCadastrada)
+        {
+            InstituicaoRepository.Cadastrar(InstituicaoCadastrada);
+            return Ok();
+        }
+        
+    }
+}
---
+++ VisualStudio/Senai.SviGufo.WebApi/Senai.SviGufo.WebApi/Domains/InstituicaoDomain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.SviGufo.WebApi.Domains
+{
+    public class InstituicaoDomain
+    {
+        public int Id { get; set;}
+
+        public string Nome_Fantasia { get; set; }
+
+        public string Razao_Social { get; set; }
+
+        public string CNPJ { get; set; }
+
+        public string Logradouro { get; set; }
+
+        public string CEP { get; set; }
+
+        public string UF { get; set; }
+
+        public string Cidade { get; set; }
+    }
+}
